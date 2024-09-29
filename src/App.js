b3a54d655116f8c2492a60852aff140990668e5f
ahwathy <aswathiachus935@gmail.com>
2024-09-29 21:25:15 +0530

    add color

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@
 
 function App() {
   return (
-    <h1>hello world
-      
-    </h1>
+    <div>
+    <h1 style={{backgroundColor:"grey"}}>hello world</h1>
+    <p>Sample react</p>
+    </div>
   );
 }
 
